cff41b45563543eeb2c2145e1779ba83fd9f68fb
nagpursai <krishj2ee9@gmail.com>
2017-05-16 04:44:42 -0400

    update

---
+++ src/main/java/com/sai/Cheque.java
@@ -0,0 +1,16 @@
+package com.sai;
+
+
+public class Cheque extends Payment{
+ 
+	private String ChequeType;
+ 
+	public String getChequeType() {
+		return ChequeType;
+	}
+ 
+	public void setChequeType(String chequeType) {
+		ChequeType = chequeType;
+	}
+ 
+}
---
+++ src/main/java/com/sai/Client.java
@@ -0,0 +1,43 @@
+package com.sai;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class Client {
+	
+	 
+	
+	    public static void main(String[] args)
+	    {
+	 
+	        Configuration cfg = new Configuration();
+	        cfg.configure("hibernate.cfg.xml"); 
+	 
+	        SessionFactory factory = cfg.buildSessionFactory();
+	        Session session = factory.openSession();
+	 
+	        CreditCard c=new CreditCard();
+	 
+	        c.setPaymentId(13);
+	        c.setAmount(2500);
+	        c.setCreditCardType("Visa");
+	 
+	 
+	        
+	        Cheque c1=new Cheque();
+	        
+	        c1.setPaymentId(14);
+	        c1.setAmount(2600);
+	        c1.setChequeType("ICICI");
+	 
+	        
+	        
+	        Transaction tx = session.beginTransaction();
+	        session.save(c);
+	        session.save(c1);
+	        System.out.println("Object saved successfully.....!!");
+	        tx.commit();
+	        session.close();
+	        factory.close();
+	    }
+	 
+	}
